3898617194fcfa23f90459fc906f5addae7f2fed
Usuario <Usuario@Profesor1DAM>
2025-04-22 11:59:42 +0200

    Primer commit

--- ProyectoNetbeans/src/main/java/com/mycompany/proyectonetbeans/ProyectoNetbeans.java
+++ ProyectoNetbeans/src/main/java/com/mycompany/proyectonetbeans/ProyectoNetbeans.java
@@ -12,5 +12,6 @@ public class ProyectoNetbeans {
 
     public static void main(String[] args) {
         System.out.println("Primer commit");
+        System.out.println("Primer commit");
     }
 }
